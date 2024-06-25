81403c58cf3e5ba6547e1ceee148daae835094fa
NowhereMan3 <147972435+NowhereMan3@users.noreply.github.com>
2024-06-25 18:53:45 +0300

    Update celltowerssmap.js

--- js/celltowerssmap.js
+++ js/celltowerssmap.js
@@ -1,1 +1,19 @@
 
+let map2 = L.map('map').setView([58.373523, 26.716045], 12)
+const osm2 =
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+ maxZoom: 19,
+ attribution: 'OpenStreetMap contributors',
+})
+osm2.addTo(map2)
+
+
+
+// add geoJSON points layer*
+async function addCelltowersGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const markers = L.geoJson(data)
+ markers.addTo(map2)
+}
+addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
