d437ac812cb9d655f0146e1d1b1b32808b8ed5ea
NowhereMan3 <147972435+NowhereMan3@users.noreply.github.com>
2024-06-25 18:40:14 +0300

    Update districtsmap.js

--- js/districtsmap.js
+++ js/districtsmap.js
@@ -1,10 +1,10 @@
-let map = L.map('map').setView([58.373523, 26.716045], 12)
+let map = L.map('map1').setView([58.373523, 26.716045], 12)
 const osm =
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
  maxZoom: 19,
  attribution: 'OpenStreetMap contributors',
 })
-osm.addTo(map)
+osm.addTo(map1)
 
 
 // add popup to each feature
@@ -19,7 +19,7 @@ async function addDistrictsGeoJson(url) {
  onEachFeature: popUPinfo,
  style: polygonStyle,
  })
- polygons.addTo(map)
+ polygons.addTo(map1)
 }
 addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
 
