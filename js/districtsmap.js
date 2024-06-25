5d2fb7b26889f3d26819efe9d80f7cd990cea772
NowhereMan3 <147972435+NowhereMan3@users.noreply.github.com>
2024-06-25 19:40:02 +0300

    Update districtsmap.js

--- js/districtsmap.js
+++ js/districtsmap.js
@@ -7,11 +7,46 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 osm1.addTo(map1)
 
 
+
+
+
+
+// get color from feature property
+function getColor(property) {
+ switch (property) {
+ case 1:
+ return '#ff0000'
+ case 13:
+ return '#009933'
+ case 6:
+ return '#0000ff'
+ case 7:
+ return '#ff0066'
+ default:
+ return '#ffffff'
+ }
+}
+
+
+// polygon style
+function polygonStyle(feature) {
+ return {
+ fillColor: getColor(feature.properties.OBJECTID),
+ fillOpacity: 0.5,
+ weight: 1,
+ opacity: 1,
+ color: 'grey',
+ }
+}
+
+
+
 // add popup to each feature
 function popUPinfo(feature, layer) {
  layer.bindPopup(feature.properties.NIMI)
 }
-// add geoJSON polygons layer
+
+
 async function addDistrictsGeoJson(url) {
  const response = await fetch(url)
  const data = await response.json()
